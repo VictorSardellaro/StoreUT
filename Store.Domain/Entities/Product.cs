8296017010108e0575a427f38b3f2ef612bda3c5
VictorSardellaro <victor.henryk@outlook.com>
2021-10-27 11:19:51 -0300

    Discount Entity

--- Store.Domain/Entities/Product.cs
+++ Store.Domain/Entities/Product.cs
@@ -12,5 +12,6 @@ namespace Store.Domain.Entities
         public string Title { get; private set; }
         public decimal Price { get; private set; }
         public bool Active { get; private set; }
+
     }
 }
