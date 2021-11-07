7fe496e2ebc4460c8f5b074e18925ed8e53acd92
VictorSardellaro <victor.henryk@outlook.com>
2021-11-07 20:10:25 -0300

    OrderTest

---
+++ Store.Tests/Commands/CreateOrderCommandTests.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.Commands;
+
+namespace Store.Tests.Commands
+{
+
+    [TestClass]
+    public class CreateOrderCommandTests
+    {
+        [TestMethod]
+        [TestCategory("Handlers")]
+        public void Dado_um_comando_invalido_o_pedido_nao_deve_ser_gerado()
+        {
+            var command = new CreateOrderCommand();
+            command.Customer = "";
+            command.ZipCode = "134110080";
+            command.PromoCode = "12345678";
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Validate();
+
+            Assert.AreEqual(command.Valid, false);
+        }
+
+    }
+}
+
